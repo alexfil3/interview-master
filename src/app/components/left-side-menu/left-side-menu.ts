8838991e824eceab5d7a25340dd75447b3eec9e1
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-09-21 21:12:06 +0300

    add left side menu

---
+++ src/app/components/left-side-menu/left-side-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-side-menu',
+  imports: [],
+  templateUrl: './left-side-menu.html',
+  styleUrl: './left-side-menu.scss'
+})
+export class LeftSideMenu {
+
+}
