eb2c03884180280b498cf25295f9f0a17e70dcde
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-09-21 21:21:25 +0300

    add global styles and html to left side nav

--- src/app/components/left-side-menu/left-side-menu.ts
+++ src/app/components/left-side-menu/left-side-menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-left-side-menu',
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './left-side-menu.html',
   styleUrl: './left-side-menu.scss'
 })
