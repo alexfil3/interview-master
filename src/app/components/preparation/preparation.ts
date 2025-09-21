e48931ca4bf77a483834a706c8bd364f15313389
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-09-21 21:16:07 +0300

    add preparation component

---
+++ src/app/components/preparation/preparation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preparation',
+  imports: [],
+  templateUrl: './preparation.html',
+  styleUrl: './preparation.scss'
+})
+export class Preparation {
+
+}
